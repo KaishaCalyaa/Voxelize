4afd0631d4ccb171de60c962003db09da2f9a17f
KaishaCalyaa <calyakaisha804@gmail.com>
2025-12-04 12:30:28 +0700

    first upload

---
+++ edu-box-app-master/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'pattern',
+    loadChildren: () => import('./pattern/pattern.module').then(m => m.PatternPageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then(m => m.SettingsPageModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { 
+      preloadingStrategy: PreloadAllModules,
+      enableTracing: false
+    })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
